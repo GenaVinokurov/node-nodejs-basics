10c2dfbc4358c3eedc022a13e0f57a9d1628cd44
gennady <gennady.vinokurov@innowise.com>
2025-10-26 13:57:39 +0100

    feat: streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+
+  const writeStream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(writeStream);
+
+  return new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve);
+    writeStream.on("error", reject);
+  });
 };
+
+write();
